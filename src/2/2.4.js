a10eed746b259ab59490a6ec47c7affba3629fc5
Matt Mapplebeck <mmapplebeck@about.com>
2017-04-24 14:32:14 -0400

    tabs to spaces

--- src/2/2.4.js
+++ src/2/2.4.js
@@ -1,14 +1,14 @@
 import Node from '../structures/LinkedList'
 
 export const partition = (linkedList, value) => {
-	let head = linkedList
-	while(head !== null) {
-		let next = head.next
-		if (head.data < value) {
-			head.next = linkedList
-			linkedList = head
-		}
-		head = next
-	}
-	return linkedList
+    let head = linkedList
+    while(head !== null) {
+        let next = head.next
+        if (head.data < value) {
+            head.next = linkedList
+            linkedList = head
+        }
+        head = next
+    }
+    return linkedList
 }
