97e571b3fa461ec66e0fbfab3834f492edd6dbbf
Matt Mapplebeck <mmapplebeck@about.com>
2017-04-26 14:00:45 -0400

    add 2.5 followup / forward sum lists

--- src/2/2.5.js
+++ src/2/2.5.js
@@ -15,3 +15,61 @@ export const sumLists = (first, second, carry = 0) => {
   }
   return summed
 }
+
+export const getLength = list => {
+  let head = list
+  let length = 1
+  while(head.next !== null) {
+    length += 1
+    head = head.next
+  }
+  return length
+}
+
+export const padList = (list, n) => {
+  for(let i = 0; i < n; i++) {
+    let temp = list
+    list = new Node(0)
+    list.next = temp
+  }
+  return list
+}
+
+export const appendToList = (list, data) => {
+  let node = new Node(data)
+  if (list !== null) {
+    node.next = list
+  }
+  return node
+}
+
+export const getPartialSum = (first, second) => {
+  if(first == null && second == null) {
+    return {
+      list: null,
+      carry: 0
+    }
+  }
+  let partialSum = getPartialSum(first.next, second.next)
+  let sum = first.data + second.data + partialSum.carry
+  return {
+    list: appendToList(partialSum.list, sum % 10),
+    carry: sum >= 10 ? 1 : 0
+  }
+}
+
+export const sumListsForward = (first, second, prev = null) => {
+  if (first === null && second === null) return null
+  const l1 = getLength(first)
+  const l2 = getLength(second)
+  if (l1 < l2) {
+    first = padList(first, l2 - l1)
+  } else {
+    second = padList(second, l1 - l2)
+  }
+  let partialSum = getPartialSum(first, second)
+  if (partialSum.carry === 1) {
+    return appendToList(partialSum.list, 1)
+  }
+  return partialSum.list
+}
--- src/2/2.5.spec.js
+++ src/2/2.5.spec.js
@@ -1,6 +1,6 @@
 import expect from 'expect'
 import Node from '../structures/LinkedList'
-import {sumLists} from './2.5'
+import * as funcs from './2.5'
 
 describe('2.5', () => {
 
@@ -30,7 +30,93 @@ describe('2.5', () => {
         .appendToTail(0)
         .appendToTail(1)
 
-      const actual = sumLists(first, second)
+      const actual = funcs.sumLists(first, second)
+
+      expect(actual).toEqual(expected)
+    })
+  })
+
+  describe('getLength()', () => {
+
+    it('should get the length of the list', () => {
+      const list = new Node(1)
+        .appendToTail(2)
+        .appendToTail(3)
+
+      expect(funcs.getLength(list)).toEqual(3)
+    })
+  })
+
+  describe('padList()', () => {
+
+    it('should pad the list with 0 n times', () => {
+      const list = new Node(1)
+      const expected = new Node(0)
+        .appendToTail(0)
+        .appendToTail(1)
+      const actual = funcs.padList(list, 2)
+      expect(actual).toEqual(expected)
+    })
+  })
+
+  describe('appendToList()', () => {
+
+    it('should append a node to the list', () => {
+      const list = new Node(1)
+      const expected = new Node(2).appendToTail(1)
+      const actual = funcs.appendToList(list, 2)
+      expect(actual).toEqual(expected)
+    })
+  })
+
+  describe('getPartialSum()', () => {
+
+    it('should handle null input and return defaults', () => {
+      const expected = {
+        list: null,
+        carry: 0
+      }
+      const actual = funcs.getPartialSum(null, null)
+      expect(actual).toEqual(expected)
+    })
+
+    it('should return list and carry', () => {
+      const expected = {
+        list: new Node(1),
+        carry: 1
+      }
+      const actual = funcs.getPartialSum(new Node(9), new Node(2))
+      expect(actual).toEqual(expected)
+    })
+  })
+
+  describe('sumListsForward()', () => {
+
+    it('should sum the lists', () => {
+
+      // 123
+      const first = new Node(1)
+        .appendToTail(2)
+        .appendToTail(3)
+
+      // 9898
+      const second = new Node(9)
+        .appendToTail(8)
+        .appendToTail(9)
+        .appendToTail(8)
+
+      //   0123
+      // + 9898
+      //-------
+      //  10021 
+
+      const expected = new Node(1)
+        .appendToTail(0)
+        .appendToTail(0)
+        .appendToTail(2)
+        .appendToTail(1)
+
+      const actual = funcs.sumListsForward(first, second)
 
       expect(actual).toEqual(expected)
     })
