2ebc2e21c7fa947f5f09eedefc4fa26897aebd30
Matt Mapplebeck <matt.mapplebeck@gmail.com>
2017-06-06 15:49:17 -0400

    add extra assertions

--- src/1/1.4.spec.js
+++ src/1/1.4.spec.js
@@ -6,12 +6,26 @@ describe('1.4', () => {
   describe('isPermutationPalindrome()', () => {
 
     it('should return true if permutation is palindrome', () => {
+      // ababa
+      expect(isPermutationPalindrome('bbaaa')).toEqual(true)
+
       // abcba
       expect(isPermutationPalindrome('aabbc')).toEqual(true)
+
+      // abba
+      expect(isPermutationPalindrome('aabb')).toEqual(true)
+
+      // aba
+      expect(isPermutationPalindrome('aab')).toEqual(true)
+
+      expect(isPermutationPalindrome('aba')).toEqual(true)
+      expect(isPermutationPalindrome('aa')).toEqual(true)
+      expect(isPermutationPalindrome('a')).toEqual(true)
     })
 
     it('should return false if permutation is not palindrome', () => {
       expect(isPermutationPalindrome('aabbcd')).toEqual(false)
+      expect(isPermutationPalindrome('abc')).toEqual(false)
     })
   })
 })
