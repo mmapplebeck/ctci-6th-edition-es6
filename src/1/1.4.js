a7f7c1edff809eedf87aea3ebc5a442b267b2107
Matt Mapplebeck <matt.mapplebeck@gmail.com>
2017-06-07 10:26:05 -0400

    simplify odd count logic

--- src/1/1.4.js
+++ src/1/1.4.js
@@ -1,19 +1,10 @@
 export const isPermutationPalindrome = str => {
-  let counts = {}
+  let oddLetters = {}
   let oddCount = 0
   let letters = str.split(' ').join('')
   for (let i = 0; i < letters.length; i++) {
-    if (counts.hasOwnProperty(letters[i])) {
-      counts[letters[i]] += 1
-      if (counts[letters[i]] % 2 === 0) {
-        oddCount -= 1
-      } else {
-        oddCount += 1
-      }
-    } else {
-      counts[letters[i]] = 1
-      oddCount += 1
-    }
+    oddLetters[letters[i]] = oddLetters[letters[i]] ? !oddLetters[letters[i]] : true
+    oddCount += oddLetters[letters[i]] ? 1 : -1
   }
   return oddCount <= 1
 }
