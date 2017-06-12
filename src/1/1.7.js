022effe6da5cd6fce867bee13c18ad46f30d5edc
Matt Mapplebeck <matt.mapplebeck@gmail.com>
2017-06-11 21:10:16 -0400

    add a rotate in place variation

--- src/1/1.7.js
+++ src/1/1.7.js
@@ -8,3 +8,16 @@ export const rotate = matrix => {
   }
   return rotated
 }
+
+export const rotateInPlace = matrix => {
+  for (let i = 0; i < matrix.length / 2; i ++) {
+    for (let j = i; j < matrix.length - 1 - i; j++) {
+      let temp = matrix[i][j]
+      matrix[i][j] = matrix[matrix.length - 1 - j][i]
+      matrix[matrix.length - 1 - j][i] = matrix[matrix.length - 1 - i][matrix.length - 1 - j]
+      matrix[matrix.length - 1 - i][matrix.length - 1 - j] = matrix[j][matrix.length - 1 - i]
+      matrix[j][matrix.length - 1 - i] = temp
+    }
+  }
+  return matrix
+}
--- src/1/1.7.spec.js
+++ src/1/1.7.spec.js
@@ -1,22 +1,33 @@
 import expect from 'expect'
-import {rotate} from './1.7'
+import {rotate, rotateInPlace} from './1.7'
 
 describe('1.7', () => {
 
-  describe('rotate', () => {
+  const testRotate = fn => {
+    const matrix = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ]
+    const expected = [
+      [7, 4, 1],
+      [8, 5, 2],
+      [9, 6, 3]
+    ]
+    expect(fn(matrix)).toEqual(expected)
+  }
+
+  describe('rotate()', () => {
 
     it('should rotate the matrix 90 degrees', () => {
-      const matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-      ]
-      const expected = [
-        [7, 4, 1],
-        [8, 5, 2],
-        [9, 6, 3]
-      ]
-      expect(rotate(matrix)).toEqual(expected)
+      testRotate(rotate)
+    })
+  })
+
+  describe('rotateInPlace()', () => {
+
+    it('should rotate the matrix 90 degrees', () => {
+      testRotate(rotateInPlace)
     })
   })
 })
