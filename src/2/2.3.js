6d87a811cd2e8f903c1518f17665ab0c79d69bc5
Matt Mapplebeck <matt.mapplebeck@gmail.com>
2017-06-15 14:42:12 -0400

    practice 2.3

--- src/2/2.3.js
+++ src/2/2.3.js
@@ -1,6 +1,6 @@
 import Node from '../structures/LinkedList'
 
 export const deleteMiddleNode = node => {
-    node.data = node.next.data
-    node.next = node.next.next
+  node.data = node.next.data
+  node.next = node.next.next
 }
