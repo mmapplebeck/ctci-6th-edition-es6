edd4ff16c6417734659ebaaf8b65f5cc6728aea4
Matt Mapplebeck <matt.mapplebeck@gmail.com>
2017-06-22 22:27:28 -0400

    redo 2.4

--- src/2/2.4.js
+++ src/2/2.4.js
@@ -1,14 +1,39 @@
 import Node from '../structures/LinkedList'
 
-export const partition = (linkedList, value) => {
-    let head = linkedList
-    while(head !== null) {
-        let next = head.next
-        if (head.data < value) {
-            head.next = linkedList
-            linkedList = head
-        }
-        head = next
+export const partition = (linkedList, partition) => {
+  let beforeStart = null
+  let beforeEnd = null
+  let afterStart = null
+  let afterEnd = null
+
+  let head = linkedList
+
+  while (head) {
+    if (head.data < partition) {
+      if (beforeStart === null) {
+        beforeStart = head
+        beforeEnd = beforeStart
+      } else {
+        beforeEnd.next = head
+        beforeEnd = head
+      }
+    } else {
+      if (afterStart === null) {
+        afterStart = head
+        afterEnd = afterStart
+      } else {
+        afterEnd.next = head
+        afterEnd = head
+      }
     }
-    return linkedList
+    head = head.next
+  }
+
+  afterEnd.next = null
+  
+  if (beforeEnd === null) return afterStart
+
+  beforeEnd.next = afterStart
+
+  return beforeStart
 }
